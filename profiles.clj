8a65dc546aa52dddc1c32dd4ffd460b524749f33
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-01-03 18:26:52 -0500

    Switch cider-nrepl to 0.11.0-SNAPSHOT, add explicit dependency to nrepl to override Garden nrepl version, update some plugins

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]
+{:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
                   [lein-kibit "0.1.2"]
-                  [lein-ancient "0.6.7"]
-                  [jonase/eastwood "0.2.1"]]}}
+                  [lein-ancient "0.6.8"]
+                  [jonase/eastwood "0.2.3"]]}}
