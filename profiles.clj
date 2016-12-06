05c5e3f25071a5d6a6801114fec649073e2080b2
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-12-05 23:09:13 -0500

    Add ultra for improved REPL

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-kibit "0.1.2"]
                   [lein-ancient "0.6.10"]
-                  [jonase/eastwood "0.2.3"]]}}
+                  [jonase/eastwood "0.2.3"]
+                  [venantius/ultra "0.5.0"]]}}
