cc2706594da58cbb46beb7f48c2ca3768d433c0b
Marc-André Goyette <goyette.marcandre@gmail.com>
2017-01-25 20:54:58 -0500

    Update plugins

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
-{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.18.0"]
+{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]
                   [lein-ancient "0.6.10"]
-                  [lein-kibit "0.1.2"]
+                  [lein-kibit "0.1.3"]
                   [jonase/eastwood "0.2.3"]
-                  [venantius/ultra "0.5.0"]]}}
+                  [venantius/ultra "0.5.1"]]}}
