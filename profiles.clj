848ecf2a94ca677796ac9494cc2641ed2360ecde
magoyette <goyette.marcandre@gmail.com>
2018-10-08 22:21:22 -0400

    Remove ultra because of conflicts with CIDER, upgrade dependencies

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,4 @@
-{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]
-                  [lein-ancient "0.6.10"]
-                  [lein-kibit "0.1.3"]
-                  [jonase/eastwood "0.2.3"]
-                  [venantius/ultra "0.5.1"]]}}
+{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]
+                  [lein-ancient "0.6.15"]
+                  [lein-kibit "0.1.6"]
+                  [jonase/eastwood "0.3.1"]]}}
