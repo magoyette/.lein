d099ee354de335a11e9a812a50ab264465076024
magoyette <goyette.marcandre@gmail.com>
2018-11-25 09:52:08 -0500

    Upgrade eastwood

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]
                   [lein-ancient "0.6.15"]
                   [lein-kibit "0.1.6"]
-                  [jonase/eastwood "0.3.1"]]}}
+                  [jonase/eastwood "0.3.3"]]}}
