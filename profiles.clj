d641585da229db78da58961160c35bf99f5c1bda
magoyette <goyette.marcandre@gmail.com>
2019-12-14 16:12:25 -0500

    Upgrade lein-kibit

--- profiles.clj
+++ profiles.clj
@@ -2,5 +2,5 @@
         :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]}
         :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
                   [lein-ancient "0.6.15"]
-                  [lein-kibit "0.1.6"]
+                  [lein-kibit "0.1.8"]
                   [jonase/eastwood "0.3.6"]]}}
