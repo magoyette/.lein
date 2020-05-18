33829257bf85fa5690d3d0e8145a2bdbd215eac4
Marc-André Goyette <goyette.marcandre@gmail.com>
2020-05-17 23:27:36 -0400

    Upgrade eastwood

--- profiles.clj
+++ profiles.clj
@@ -3,4 +3,4 @@
         :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
                   [lein-ancient "0.6.15"]
                   [lein-kibit "0.1.8"]
-                  [jonase/eastwood "0.3.6"]]}}
+                  [jonase/eastwood "0.3.10"]]}}
